26015497643296b2ad3dd9fbf16c4cfca03569b7
e <cguiselin@norauto.fr>
2020-11-30 17:11:21 +0100

    récupmailetIdetintegrationsuruserdansmongodb

--- src/server.js
+++ src/server.js
@@ -132,7 +132,10 @@ function makeApp(mongoClient) {
   })
 
   //  annonce qui se retrouve sur la page la location (
-  app.get("/api/creation_annonce", async (req, res) => {
+  app.get("/api/creation_annonce", sessionParser, async (req, res) => {
+
+    console.log(req.session.mail)
+    
     // const result = " veuillez vous logger";
     // await db.collection("").findOne;
     // console.log(result);
@@ -213,11 +216,13 @@ function makeApp(mongoClient) {
       console.log(
         "warning, couldn't put the tokens in session, vous devez créer un compte"
       );
+      req.session.accessToken = token.access_token;
+      req.session.mail = dataNewUser.email
     }
     res.redirect("/");
   });
 
-  app.post("/api/creation_annonce", async (req, res) => {
+  app.post("/api/creation_annonce",sessionParser, async (req, res) => {
     const dataForm = req.body;
     const annonce = {
       titre: dataForm.titre,
@@ -234,6 +239,24 @@ function makeApp(mongoClient) {
 
     const result = await db.collection("Annonces").insertOne(annonce);
     const createdId = result.insertedId;
+
+
+    const logguedUserEmail = req.session.mail;
+    // const user = await db.collection("users").findOne({mail:logguedUserEmail});
+    const Id = await db.collection("Users").updateOne({mail:logguedUserEmail}, { $push: {annonce_vendeur: createdId}});
+
+    console.log(Id)
+
+    // trouver le user dans la collection Users
+
+    console.log("j'ai reussi");
+
+    // trouver dans mongodb comment patch un tableau de donnee
+    // dans le user en question : rajouter l'id de l'annonce dans le tableau dans "annonce_vendeur"
+
+  
+    res.redirect("/");
+    // });
 
 //     var cookieSession = require('cookie-session');
 //     app.use(cookieSession({
